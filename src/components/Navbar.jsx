bbdc006404ee0481844bae1acea79de38394fd1f
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-04-30 16:06:33 +0530

    making the responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,28 @@
-import { LiaBarsSolid } from "react-icons/lia";
+import { LiaBarsSolid, LiaTimesSolid } from "react-icons/lia";
 import '../styles/navbar.css';
+import { useState } from "react";
 
 
 export default function Navbar(){
+    const [menuClicked, setMenuClicked] = useState(false);
+
+
     return(
             <nav>
                 <div className="nav-left">
-                    <ul>
+                    <ul className= {menuClicked ? "nav-left-ul active" : "nav-left-ul"}>
                         <li>HOME</li>
                         <li>PROJECTS</li>
                         <li>SERVICES</li>
                         <li>SKILLS</li>
                         <li>STATS</li>
+                        <li className="connect-btn-li">CONNECT</li>
                     </ul>
                 </div>
                 <div className="nav-right">
-                    <button>CONNECT</button>
-                    <LiaBarsSolid color="#F2613F" className="menu-icon"/>
+                    <button className="connect-btn">CONNECT</button>
+                    {!menuClicked && <LiaBarsSolid color="#F2613F" className="menu-icon" size={40} onClick={()=>{setMenuClicked(!menuClicked)}} tra/>}
+                    {menuClicked && <LiaTimesSolid color="#F2613F" className="menu-icon" size={40} onClick={()=>{setMenuClicked(!menuClicked)}} />}
                 </div>
             </nav>
     )
