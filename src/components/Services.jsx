9d7b184764c448c92c56d4c6188ed55b4ceab851
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-05-02 10:23:49 +0530

    making the services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,40 @@
+import { TbWriting } from 'react-icons/tb';
 import '../styles/services.css';
+import { MdApi, MdPermIdentity } from 'react-icons/md';
+import { SiAffinitydesigner, SiPowerpages } from 'react-icons/si';
+import { CgWebsite } from 'react-icons/cg';
+import React from 'react';
 
 export default function Services(){
     const services= "SERVICES";
     const servicesArray = services.split("");
+
+    const iconArray = [
+        TbWriting,
+        MdApi,
+        SiAffinitydesigner,
+        CgWebsite,
+        SiPowerpages,
+        MdPermIdentity    
+    ]
+
+    const servicesHeadings = [
+        "Content Writing",
+        "REST API",
+        "Graphic Designing",
+        "Web Applications",
+        "Landing Pages",
+        "Portfolio Sites"
+    ]
+
+    const servicesSubHeadings = [
+        "I can write blogs, articles, documentations and content that fits your applications.",
+        "i am good at developing secure aPI using spring boot 3 with spring security and jWT token.",
+        "using canva and Photoshop i can create stunning, posters, pamphlets, designs and much more. ",
+        "web applications that will help you bring your ideas in reality!",
+        "pages that are tailored specifically for your business including SEO",
+        "let me take care of showcasing your profession over the internet!"
+    ]
 
     return(
         <div className="services">
@@ -11,7 +43,24 @@ export default function Services(){
             </div>
             <div className="bottom">
                 <div className='left'>
-                    hello
+
+                {
+                        (() => {
+                            const elements = [];
+                            for (let i = 0; i < iconArray.length-3; i++) {
+                                elements.push(
+                                    <div className='service-l'>
+                                        <div className='service-l-text'>
+                                            <h1 className='montserrat-regular service-l-head'>{servicesHeadings[i]}</h1>
+                                            <h2 className='montserrat-light service-l-subhead'>{servicesSubHeadings[i]}</h2>
+                                        </div>
+                                        {React.createElement(iconArray[i], { className: 'service-l-icon' })}
+                                    </div>
+                                );
+                            }
+                            return elements;
+                        })()
+                    }
                 </div>
                 <div className='middle'>
                     {
@@ -24,7 +73,23 @@ export default function Services(){
                     }
                 </div>
                 <div className='right'>
-                    hello
+                    {
+                        (() => {
+                            const elements = [];
+                            for (let i = 3; i < iconArray.length; i++) {
+                                elements.push(
+                                    <div className='service-r' key={i}>
+                                        {React.createElement(iconArray[i], { className: 'service-r-icon' })}
+                                        <div className='service-r-text'>
+                                            <h1 className='montserrat-regular service-r-head'>{servicesHeadings[i]}</h1>
+                                            <h2 className='montserrat-light service-r-subhead'>{servicesSubHeadings[i]}</h2>
+                                        </div>
+                                    </div>
+                                );
+                            }
+                            return elements;
+                        })()
+                    }
                 </div>
             </div>
         </div>
